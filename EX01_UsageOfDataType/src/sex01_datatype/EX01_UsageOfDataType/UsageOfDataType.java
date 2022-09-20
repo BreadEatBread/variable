13a242cd99d0411d21a54de0f59fa0a05e6b5c62
BreadEatBread <woong4252@gmail.com>
2022-09-20 14:23:44 +0900

    Intial commit

---
+++ EX01_UsageOfDataType/src/sex01_datatype/EX01_UsageOfDataType/UsageOfDataType.java
@@ -0,0 +1,13 @@
+package sex01_datatype.EX01_UsageOfDataType;
+
+public class UsageOfDataType {
+	public static void main(String[] srgs) {
+		int a =3;
+		int b;
+		b=4;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
